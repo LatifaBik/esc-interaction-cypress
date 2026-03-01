529b030227fea97722512df95ba79c30e7991a8e
LatifaBik <latifa.bikndarne@lerniastudent.se>
2026-03-01 10:38:06 +0100

    Add homepage E2E test (G)

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,10 +1,13 @@
 describe("Cypress E2E test - Hacker Escape Rooms", () => {
-  it("Sidan är uppe på localhost och visar korrekt H1", () => {
-    cy.visit("/");                 
-    cy.location("hostname").should("eq", "localhost");
+ 
+it("Sidan är uppe och visar korrekt H1", () => {
+  cy.visit("/");
 
-    // Kontrollerar statisk text i H1 
-    cy.get("h1").should("be.visible");
-    cy.get("h1").should("contain.text", "Hacker Escape Room");
-  });
+  const expectedHost = new URL(Cypress.config("baseUrl")).hostname;
+  cy.location("hostname").should("eq", expectedHost);
+
+  cy.get("h1").should("be.visible");
+  cy.get("h1").should("contain.text", "Hacker Escape Room"); 
 });
+});
+
