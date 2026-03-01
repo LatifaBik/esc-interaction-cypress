2cd1200ff42800875da1f5cd00a599e29618e42a
LatifaBik <latifa.bikndarne@lerniastudent.se>
2026-03-01 10:39:05 +0100

    Configure Cypress to support multiple environments

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,11 @@
+
+
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  allowCypressEnv: false,
   e2e: {
-    baseUrl: "http://localhost:5500",
-
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: process.env.CYPRESS_BASE_URL || "http://localhost:5500",
   },
 });
+
+
